cdcd4bb91b218557599af86e94d9b7052a360d45
okezieobi <obiedereokezie@gmail.com>
2020-05-07 16:10:56 +0100

    Updates

---
+++ precache-manifest.a8a55199b4f34a51d75c5404a0afb158.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3f95aef98b3bf8bfb66cacde4abf8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eebad1db0fd430de9ed2",
+    "url": "/static/css/main.374cc278.chunk.css"
+  },
+  {
+    "revision": "ec77d2d32f6b67e5cbae",
+    "url": "/static/js/2.37b1df5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.37b1df5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eebad1db0fd430de9ed2",
+    "url": "/static/js/main.df1af388.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
